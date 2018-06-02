67073ef6cf8f5cf28afc78564a49da9dfadb1937
Cristian Duartes <c.duartes.dn@gmail.com>
2018-06-01 20:27:13 -0400

    fix problems

--- main/urls.py
+++ main/urls.py
@@ -1,16 +1,19 @@
-from django.urls import path
-from django.conf.urls import url
-from . import views
 from django.contrib.auth import views as auth_views
+from django.conf.urls import url
+from django.urls import path
+from . import views
 
 urlpatterns = [
+    # simple url
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    #url(r'^login/$', auth_views.login, name='login'),
-    #url(r'^logout/$', auth_views.logout, name='logout'),
+
+    # main site
+    path('', views.root),
     path('home/', views.index, name='home'),
     path('tareas/',views.tareas, name='tareas'),
 
+    ## Tareas administration
     path('crear_tarea', views.crear_tarea, name="crear_tarea"),
     path('calendario', views.calendario, name="calendario"),
     path('tarea', views.tarea, name="tarea"),
--- main/views.py
+++ main/views.py
@@ -1,24 +1,25 @@
+from django.views.decorators.http import require_GET, require_POST
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group, User
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required, permission_required
-from django.views.decorators.http import require_GET, require_POST
-from django.contrib.auth.models import Group, User
 from .models import Tarea, Estado
-from datetime import date
-from django.utils import formats
 
 # Create your views here.
+def root(request):
+    return redirect('tareas')
+
 @login_required()
 def index(request):
     return redirect('tareas')
-    #return HttpResponse("HolaMundo Django!")
 
 """
-retorna todas las tareas.
-Si el usuario autentificado es administrador del sitio, retorna todas las tareas.
-Si el usuario autentificado no es administrador, retorna solo las tareas que él ha creado.
+Display all Tareas
+
+If authentificated user is admin of site, return all Tareas.
+Else, just return Tareas he owns.
 ---
-La vista mostrará por defecto solo las tareas activas, ocultando las no activas a menos que se reciba el parámetro "all=TRUE"
+View will only show active Tareas, hiding others untill the GET parameter "all=TRUE" is send
 """
 @login_required
 @require_GET
@@ -43,8 +44,10 @@ def tareas(request):
     else:
         return render(request, 'tareas.html', { 'tareas': tareas, 'estados': estados, 'all': all_tareas})
 
+"""
+Create Tarea
+"""
 @login_required
-@permission_required('main.can_add_tarea')
 @require_POST
 def crear_tarea(request):
     #tarea = Tarea.objects.create() #crea un elemento automaticamente en la bd con valores nulos
@@ -58,12 +61,13 @@ def crear_tarea(request):
     tarea.save()
     return redirect('tareas')
 
+"""
+Update Tarea's data only if user is owner.
+"""
 @login_required
-@permission_required('main.can_change_tarea')
 @require_POST
 def actualizar_tarea(request):
     tarea = Tarea.objects.get(pk=request.POST.get("hidden_id"))
-    #tarea = Tarea.objects.filter(id = request.POST.get('hidden_id'))
     if tarea.usuario == request.user:
         tarea.titulo = request.POST.get('titulo')
         tarea.descripcion = request.POST.get('descripcion')
@@ -73,29 +77,43 @@ def actualizar_tarea(request):
         tarea.save()
     return redirect('tareas')
 
+"""
+Delete specific tarea, only if authentificated user is owner or admin
+"""
 @login_required
-@permission_required('main.can_delete_tarea')
 @require_GET
 def eliminar_tarea(request):
+    admin_bool = request.user.groups.filter(name='usuario_admin').exists()
     tarea = Tarea.objects.get(pk=request.GET.get("id"))
-    if tarea.usuario == request.user:
+    if admin_bool:
+        tarea.delete()
+    elif tarea.usuario == request.user:
         tarea.delete()
     return redirect('tareas')
 
+"""
+Display as calender all Tareas if user is admin.
+Else, show only Tareas owned.
+"""
 @login_required
 def calendario(request):
     admin = request.user.groups.filter(name='usuario_admin').exists()
     if admin:
-        tareas = Tarea.objects.all()
+        tareas = Tarea.objects.filter(estado = 1)
         return render(request, 'calendario_admin.html', { 'tareas': tareas})
     else:
-        tareas = Tarea.objects.filter(usuario = request.user)
+        tareas = Tarea.objects.filter(usuario = request.user, estado = 1)
         return render(request, 'calendario.html', { 'tareas': tareas})
 
+"""
+Display data of a single Tarea
+"""
 @login_required
-@permission_required('main.can_change_tarea')
 @require_GET
 def tarea(request):
     estados = Estado.objects.all().order_by('nombre')
     tarea = Tarea.objects.get(pk=request.GET.get("id"))
-    return render(request, 'tarea.html', { 'tarea': tarea, 'estados': estados })
+    if request.user == tarea.usuario:
+        return render(request, 'tarea.html', { 'tarea': tarea, 'estados': estados })
+    else:
+        return HttpResponse("Error al obtener elemento. Credenciales no coinciden.")
